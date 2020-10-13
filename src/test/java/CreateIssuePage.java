8ea56a891754c571db1542202c11e78dd7561c48
Dozsa Peter <petinho96@gmail.com>
2020-10-13 09:54:40 +0200

    parameterized create issue

---
+++ src/test/java/CreateIssuePage.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CreateIssuePage extends PageObject {
+
+    @FindBy(id = "project-field")
+    private WebElement projectField;
+
+    @FindBy(id = "issuetype-field")
+    private WebElement issueField;
+
+    public CreateIssuePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickOnProjectField(){
+        clickOn(projectField);
+    }
+
+    public void setProjectField(String projectName){
+        projectField.sendKeys(Keys.DELETE);
+        projectField.sendKeys(projectName);
+        projectField.sendKeys(Keys.ENTER);
+    }
+
+    public void setIssueField(String issueType){
+        issueField.sendKeys(Keys.DELETE);
+        issueField.sendKeys(issueType);
+        issueField.sendKeys(Keys.ENTER);
+    }
+
+    @ParameterizedTest()
+    @CsvFileSource(resources = "/CreateIssueData.csv", numLinesToSkip = 1)
+    public void setValues(String projectName, String issueType){
+        clickOnProjectField();
+        setProjectField(projectName);
+        clickOnIssueField();
+        setIssueField(issueType);
+    }
+
+    public void clickOnIssueField() {
+        clickOn(issueField);
+    }
+}
--- src/test/java/TestPlan.java
+++ src/test/java/TestPlan.java
@@ -1,3 +1,7 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
@@ -7,7 +11,7 @@ import org.testng.annotations.Test;
 public class TestPlan {
     private static final WebDriver driver = new ChromeDriver();
 
-    @BeforeSuite
+    @BeforeAll
     public static void main(String[] args) {
         // ChromeDriver location set up in Utils class
         System.setProperty(Utils.WEBDRIVER, Utils.CHROME_DRIVER_LOCATION);
@@ -15,7 +19,8 @@ public class TestPlan {
 
     private static LoginPage loginPage = new LoginPage(driver);
     private static MainPage mainPage = new MainPage(driver);
-
+    private static CreateIssuePage createIssuePage = new CreateIssuePage(driver);
+/*
     @Test(testName = "Empty Project Without Summary")
     public static void emptyProjectWithoutSummary() {
         loginPage.maximizeWindow();
@@ -27,8 +32,29 @@ public class TestPlan {
 
         mainPage.clickCreateButton();
     }
+*/
+    @ParameterizedTest()
+    @CsvFileSource(resources = "/CreateIssueData.csv", numLinesToSkip = 1)
+    public static void createIssueForProject(String projectName, String issueType){
+        loginPage.maximizeWindow();
+        loginPage.openLoginPage();
 
-    @AfterSuite
+        loginPage.setUsername();
+        loginPage.setPassword();
+        loginPage.clickLoginButton();
+
+        mainPage.clickCreateButton();
+/*
+        createIssuePage.clickOnProjectField();
+        createIssuePage.setProjectField(projectName);
+        createIssuePage.clickOnIssueField();
+        createIssuePage.setIssueField(issueType);
+ */
+
+        createIssuePage.setValues();
+    }
+
+    @AfterAll
     public static void cleanUp(){
         driver.manage().deleteAllCookies();
         driver.close();
