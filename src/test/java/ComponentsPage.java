7f29a3761d0afc062045a345e9df114fb3753951
mike32n <mihaly32horvath@gmail.com>
2020-11-11 22:37:09 +0100

    element delete button fix

--- src/test/java/ComponentsPage.java
+++ src/test/java/ComponentsPage.java
@@ -25,7 +25,8 @@ public class ComponentsPage extends PageObject {
     @FindBy(linkText = "Operations")
     private WebElement dots;
 
-    @FindBy(linkText = "Delete")
+//    @FindBy(linkText = "Delete")
+    @FindBy(xpath = "//*[@id='deletecomponent_11621']")
     private WebElement deleteButton;
 
     @FindBy(xpath = "//input[@id='submit']")
