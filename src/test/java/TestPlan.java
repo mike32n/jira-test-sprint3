9613fab22019bcd800508889874aaf6e131b0e7b
mike32n <mihaly32horvath@gmail.com>
2020-11-12 11:59:51 +0100

    component test empty fields

--- src/test/java/TestPlan.java
+++ src/test/java/TestPlan.java
@@ -341,15 +341,24 @@ public class TestPlan {
             componentsPage.emptySearchField();
             componentsPage.emptyComponentField();
             componentsPage.setComponentNameField("glass test");
+            componentsPage.setDescriptionField("this is a test");
+            componentsPage.setDefaultAssigneeField("Project default (Project lead)");
+            componentsPage.clickAddButton();
+            mainPage.navigate(Utils.GLASS_URL);
+            projectConfigPageGlass.verifyNewGlassComponent();
+            projectConfigPageGlass.clickOnSideBarComponentIcon();
+            componentsPage.deleteComponent();
+            mainPage.logout();
+        } else {
+            componentsPage.setDescriptionField("this is a test");
+            componentsPage.setDefaultAssigneeField("Project default (Project lead)");
+            componentsPage.clickAddButton();
+            mainPage.navigate(Utils.GLASS_URL);
+            projectConfigPageGlass.verifyNewGlassComponent();
+            projectConfigPageGlass.clickOnSideBarComponentIcon();
+            componentsPage.deleteComponent();
+            mainPage.logout();
         }
-        componentsPage.setDescriptionField("this is a test");
-        componentsPage.setDefaultAssigneeField("Project default (Project lead)");
-        componentsPage.clickAddButton();
-        mainPage.navigate(Utils.GLASS_URL);
-        projectConfigPageGlass.verifyNewGlassComponent();
-        projectConfigPageGlass.clickOnSideBarComponentIcon();
-        componentsPage.deleteComponent();
-        mainPage.logout();
     }
 
     @Test
