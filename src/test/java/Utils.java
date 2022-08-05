aebcff1fc1f1905a8217c039516634f92615aa21
Mihaly Horvath <mihaly32horvath@gmail.com>
2022-08-05 17:45:01 +0200

    initial

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -2,12 +2,12 @@
 import java.io.IOException;
 
 public class Utils {
-    final static String BASE_URL = "https://jira.codecool.codecanvas.hu/";
+    final static String BASE_URL = "https://www.kesmarki.com/test_form";
     final static String WEBDRIVER = "webdriver.chrome.driver";
     final static String CHROME_DRIVER_LOCATION = "/usr/bin/chromedriver";
-    final static String PROFILE_PAGE = "https://jira.codecool.codecanvas.hu/secure/ViewProfile.jspa";
-    final static String ALTERNATIVE_LOGIN_PAGE = "https://jira.codecool.codecanvas.hu/secure/ViewProfile.jspa";
-    final static String GLASS_URL = "https://jira.codecool.codecanvas.hu/projects/PP1?selectedItem=com.codecanvas.glass:glass";
+//    final static String PROFILE_PAGE = "https://jira.codecool.codecanvas.hu/secure/ViewProfile.jspa";
+//    final static String ALTERNATIVE_LOGIN_PAGE = "https://jira.codecool.codecanvas.hu/secure/ViewProfile.jspa";
+//    final static String GLASS_URL = "https://jira.codecool.codecanvas.hu/projects/PP1?selectedItem=com.codecanvas.glass:glass";
     static String username;
     static String password;
 
